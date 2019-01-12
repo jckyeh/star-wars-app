9b1c26cc434e4bc32443cb686cfd9ae59645dc25
Jack Yeh <jckyeh@gmail.com>
2019-01-12 03:31:29 +0000

    added componentDidMount method to fetch from API

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
 import './app.css';
 import Navbar from './Navbar.js';
+import Card from './Card.js';
 
 class App extends Component {
+  async componentDidMount() {
+    let response = await fetch('https://swapi.co/api/films');
+    let data = response.json();
+    console.log(data)
+  }
+
   render() {
     return (
-      <Navbar />
+      <div>
+        <Navbar />
+        <div className="fl w-100">
+          <h1 className="f3 tc white">List of Star Wars Films</h1>
+        </div>
+        <Card />
+      </div>
     );
   }
 }
